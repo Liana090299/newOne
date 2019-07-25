958bb76a2187538f61968aa818516defbe562dde
liana mazayan <mazayanlian@gmail.com>
2019-07-25 22:53:44 +0400

    lastOne

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void sum(ref int x, ref int y)
+        {
+            if (x > 0)
+            {
+                x--;
+                y++;
+                sum(ref x, ref y);
+            }
+            else
+            {
+                Console.WriteLine(y);
+            }
+
+
+        }
+        static void Main(string[] args)
+        {
+
+            int a = 5;
+            int b = 20;
+            sum(ref a, ref b);
+
+
+
+
+        }
+
+
+    }
+}
